5190c15597e967d7d3db5b8d258b920373ec8a30
Andrew <andreykuruch794@gmail.com>
2020-09-27 21:16:57 +0300

    error fixed

--- main.py
+++ main.py
@@ -3,11 +3,12 @@ from itertools import zip_longest, cycle
 
 def xor_crypt_string(data, key):
     xored = ''
-    for x, y in zip_longest, cycle:
+    for x, y in zip_longest(data, cycle(key)):
         if not x:
-	    break
-	xored += chr(ord(x) ^ ord(y))
+            break
+        xored += chr(ord(x) ^ ord(y))
     return xored
 
 if __name__ == '__main__':
     print(xor_crypt_string('hello', 'key'))
+
